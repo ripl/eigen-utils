b41fb0a2bf6acc6682cd5eb2926cc1131fe2877c
aperez <aperez>
2011-03-03 19:38:36 +0000

    added eigen_utils functions
    git-svn-id: https://svn.csail.mit.edu/common_utils@29 d030852e-545f-40dc-8c57-637194663845

---
+++ src/eigen_utils.cpp
@@ -0,0 +1,22 @@
+#include "eigen_utils.hpp"
+
+namespace eigen_utils {
+
+double atan2Vec(const Eigen::Vector2d & vec)
+{
+  return atan2(vec(1), vec(0));
+}
+
+Eigen::Vector2d angleToVec(double angle)
+{
+  Eigen::Vector2d unit_vec;
+  unit_vec << cos(angle), sin(angle);
+  return unit_vec;
+}
+
+void angleToVec(double angle, Eigen::Vector2d & unit_vec)
+{
+  unit_vec << cos(angle), sin(angle);
+}
+}
+
---
+++ src/eigen_utils.hpp
@@ -0,0 +1,79 @@
+#ifndef __eigen_util_h__
+#define __eigen_util_h__
+
+#include <Eigen/Dense>
+#include <Eigen/Cholesky>
+#include <math.h>
+#include <bot_lcmgl_client/lcmgl.h>
+#include <GL/gl.h>
+
+#define eigen_dump(MAT) cout << #MAT << endl << (MAT) << endl
+
+namespace eigen_utils {
+
+double atan2Vec(const Eigen::Vector2d & vec);
+
+Eigen::Vector2d angleToVec(double angle);
+
+void angleToVec(double angle, Eigen::Vector2d & unit_vec);
+
+static inline void bot_lcmgl_vertex3d(bot_lcmgl_t * lcmgl, Eigen::Vector3d & vec)
+{
+  bot_lcmgl_vertex3d(lcmgl, vec(0), vec(1), vec(2));
+}
+
+void bot_lcmgl_cov_ellispe(bot_lcmgl_t * lcmgl, const Eigen::Matrix2d & cov, const Eigen::Vector3d & mu3d,
+    double scale = 1, bool fill = false)
+{
+  Eigen::SelfAdjointEigenSolver<Eigen::Matrix2d> eigen_solver(cov);
+  Eigen::Vector2d eig_vals = eigen_solver.eigenvalues();
+  Eigen::Matrix2d eig_vecs = eigen_solver.eigenvectors();
+
+  Eigen::Vector2d A = eig_vecs.block<2, 1> (0, 0);
+  double a = sqrt(eig_vals(0, 0)) * scale;
+  double b = sqrt(eig_vals(1, 0)) * scale;
+  double h = mu3d(0);
+  double k = mu3d(1);
+  double phi = atan2Vec(A);
+
+  if (fill)
+    bot_lcmgl_begin(lcmgl, GL_POLYGON);
+  else
+    bot_lcmgl_begin(lcmgl, GL_LINE_LOOP);
+
+  double d_theta = M_PI / 30;
+  double t = 0;
+  while (t < 2 * M_PI) {
+    double x = h + a * cos(t) * cos(phi) - b * sin(t) * sin(phi);
+    double y = k + b * sin(t) * cos(phi) + a * cos(t) * sin(phi);
+    bot_lcmgl_vertex3d(lcmgl, x, y, mu3d(2));
+    t += d_theta;
+  }
+  bot_lcmgl_end(lcmgl);
+}
+
+void bot_lcmgl_cov_ellispe(bot_lcmgl_t * lcmgl, const Eigen::Matrix2d & cov, const Eigen::Vector2d & mu2d,
+    double scale = 1, bool fill = false)
+{
+  Eigen::Vector3d xyz = Eigen::Vector3d::Zero();
+  xyz.block<2, 1> (0, 0) = mu2d;
+  bot_lcmgl_cov_ellispe(lcmgl, cov, xyz, scale, fill);
+}
+
+template<int N>
+Eigen::Matrix<double, N, 1> randn(const Eigen::Matrix<double, N, N> & cov)
+{
+  int num_samps = 12;
+  //first get independent gaussian vector
+  Eigen::Matrix<double, N, 1> vec = Eigen::Matrix<double, N, 1>::Zero();
+
+  for (int ii = 0; ii < num_samps; ii++) {
+    vec += Eigen::Matrix<double, N, 1>::Random();
+  }
+  vec /= 2;
+  Eigen::Matrix<double, N, N> chol_decomp = cov.llt().matrixL();
+  return chol_decomp * vec;
+}
+}
+
+#endif
