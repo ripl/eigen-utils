94c817ac659a8d02fdb3e879159400d027cd08e9
abry <abry>
2011-09-10 05:55:23 +0000

    finally took stuff out of the header that's not templated
    git-svn-id: https://svn.csail.mit.edu/common_utils@66 d030852e-545f-40dc-8c57-637194663845

--- src/eigen_utils.hpp
+++ src/eigen_utils.hpp
@@ -27,86 +27,24 @@ void angleToVec(double angle, Eigen::Vector2d & unit_vec);
 /*
  * returns the skew symmetric matrix corresponding to vec.cross(<other vector>)
  */
-Eigen::Matrix3d skewHat(const Eigen::Vector3d & vec)
-{
-  Eigen::Matrix3d skew_hat;
-  skew_hat << 0, -vec(2), vec(1), vec(2), 0, -vec(0), -vec(1), vec(0), 0;
-  return skew_hat;
-}
-
+Eigen::Matrix3d skewHat(const Eigen::Vector3d & vec);
 /**
  * returns the exponential coordinates of quat1 - quat2
  * (quat2.inverse() * quat1)
  */
-Eigen::Vector3d subtractQuats(const Eigen::Quaterniond & quat1, const Eigen::Quaterniond & quat2)
-{
-  Eigen::Quaterniond quat_resid = quat2.inverse() * quat1;
-  Eigen::AngleAxisd angle_axis_resid(quat_resid);
+Eigen::Vector3d subtractQuats(const Eigen::Quaterniond & quat1, const Eigen::Quaterniond & quat2);
 
-  double angle = angle_axis_resid.angle();
-  angle = bot_mod2pi(angle);
-  return angle_axis_resid.axis() * angle;
-}
+void quaternionToBotDouble(double bot_quat[4], const Eigen::Quaterniond & eig_quat);
 
-void quaternionToBotDouble(double bot_quat[4], const Eigen::Quaterniond & eig_quat)
-{
-  bot_quat[0] = eig_quat.coeffs()(3);
-  bot_quat[1] = eig_quat.coeffs()(0);
-  bot_quat[2] = eig_quat.coeffs()(1);
-  bot_quat[3] = eig_quat.coeffs()(2);
-}
+void botDoubleToQuaternion(Eigen::Quaterniond & eig_quat, const double bot_quat[4]);
 
-void botDoubleToQuaternion(Eigen::Quaterniond & eig_quat, const double bot_quat[4])
-{
-  eig_quat.coeffs()(3) = bot_quat[0];
-  eig_quat.coeffs()(0) = bot_quat[1];
-  eig_quat.coeffs()(1) = bot_quat[2];
-  eig_quat.coeffs()(2) = bot_quat[3];
-}
-
-static inline void bot_lcmgl_vertex3d(bot_lcmgl_t * lcmgl, Eigen::Vector3d & vec)
-{
-  bot_lcmgl_vertex3d(lcmgl, vec(0), vec(1), vec(2));
-}
+static inline void bot_lcmgl_vertex3d(bot_lcmgl_t * lcmgl, Eigen::Vector3d & vec);
 
 void bot_lcmgl_cov_ellispe(bot_lcmgl_t * lcmgl, const Eigen::Matrix2d & cov, const Eigen::Vector3d & mu3d,
-    double scale = 1, bool fill = false)
-{
-  Eigen::SelfAdjointEigenSolver<Eigen::Matrix2d> eigen_solver(cov);
-  Eigen::Vector2d eig_vals = eigen_solver.eigenvalues();
-  Eigen::Matrix2d eig_vecs = eigen_solver.eigenvectors();
-
-  Eigen::Vector2d A = eig_vecs.block<2, 1>(0, 0);
-  double a = sqrt(eig_vals(0, 0)) * scale;
-  double b = sqrt(eig_vals(1, 0)) * scale;
-  double h = mu3d(0);
-  double k = mu3d(1);
-  double phi = atan2Vec(A);
-
-  if (fill)
-    bot_lcmgl_begin(lcmgl, GL_POLYGON);
-  else
-    bot_lcmgl_begin(lcmgl, GL_LINE_LOOP);
-
-  double d_theta = M_PI / 30;
-  double t = 0;
-  while (t < 2 * M_PI) {
-    double x = h + a * cos(t) * cos(phi) - b * sin(t) * sin(phi);
-    double y = k + b * sin(t) * cos(phi) + a * cos(t) * sin(phi);
-    bot_lcmgl_vertex3d(lcmgl, x, y, mu3d(2));
-    t += d_theta;
-  }
-  bot_lcmgl_end(lcmgl);
-}
+    double scale = 1, bool fill = false);
 
 void bot_lcmgl_cov_ellispe(bot_lcmgl_t * lcmgl, const Eigen::Matrix2d & cov, const Eigen::Vector2d & mu2d,
-    double scale = 1, bool fill = false)
-{
-  Eigen::Vector3d xyz = Eigen::Vector3d::Zero();
-  xyz.block<2, 1>(0, 0) = mu2d;
-  bot_lcmgl_cov_ellispe(lcmgl, cov, xyz, scale, fill);
-}
-
+    double scale = 1, bool fill = false);
 /**
  * generate random normal vector with mu=0, Sigma = I
  */
