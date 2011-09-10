edcae7fb08c1bb2f02c180d6e083f646af108b1b
abry <abry>
2011-09-10 03:32:16 +0000

    added few more function, needs to be cleaned up
    git-svn-id: https://svn.csail.mit.edu/common_utils@65 d030852e-545f-40dc-8c57-637194663845

--- src/eigen_utils.hpp
+++ src/eigen_utils.hpp
@@ -3,9 +3,11 @@
 
 #include <Eigen/Dense>
 #include <Eigen/Cholesky>
+#include <Eigen/StdVector>
 #include <math.h>
 #include <iostream>
 #include <bot_lcmgl_client/lcmgl.h>
+#include <bot_core/bot_core.h>
 #ifdef __APPLE__
 #include <OpenGL/gl.h>
 #else
@@ -30,6 +32,20 @@ Eigen::Matrix3d skewHat(const Eigen::Vector3d & vec)
   Eigen::Matrix3d skew_hat;
   skew_hat << 0, -vec(2), vec(1), vec(2), 0, -vec(0), -vec(1), vec(0), 0;
   return skew_hat;
+}
+
+/**
+ * returns the exponential coordinates of quat1 - quat2
+ * (quat2.inverse() * quat1)
+ */
+Eigen::Vector3d subtractQuats(const Eigen::Quaterniond & quat1, const Eigen::Quaterniond & quat2)
+{
+  Eigen::Quaterniond quat_resid = quat2.inverse() * quat1;
+  Eigen::AngleAxisd angle_axis_resid(quat_resid);
+
+  double angle = angle_axis_resid.angle();
+  angle = bot_mod2pi(angle);
+  return angle_axis_resid.axis() * angle;
 }
 
 void quaternionToBotDouble(double bot_quat[4], const Eigen::Quaterniond & eig_quat)
@@ -60,7 +76,7 @@ void bot_lcmgl_cov_ellispe(bot_lcmgl_t * lcmgl, const Eigen::Matrix2d & cov, con
   Eigen::Vector2d eig_vals = eigen_solver.eigenvalues();
   Eigen::Matrix2d eig_vecs = eigen_solver.eigenvectors();
 
-  Eigen::Vector2d A = eig_vecs.block<2, 1> (0, 0);
+  Eigen::Vector2d A = eig_vecs.block<2, 1>(0, 0);
   double a = sqrt(eig_vals(0, 0)) * scale;
   double b = sqrt(eig_vals(1, 0)) * scale;
   double h = mu3d(0);
@@ -87,24 +103,63 @@ void bot_lcmgl_cov_ellispe(bot_lcmgl_t * lcmgl, const Eigen::Matrix2d & cov, con
     double scale = 1, bool fill = false)
 {
   Eigen::Vector3d xyz = Eigen::Vector3d::Zero();
-  xyz.block<2, 1> (0, 0) = mu2d;
+  xyz.block<2, 1>(0, 0) = mu2d;
   bot_lcmgl_cov_ellispe(lcmgl, cov, xyz, scale, fill);
+}
+
+/**
+ * generate random normal vector with mu=0, Sigma = I
+ */
+template<int N>
+void randn_identity(Eigen::Matrix<double, N, 1> & vec)
+{
+  vec = Eigen::Matrix<double, N, 1>::Zero();
+  int num_samps = 12;
+  for (int ii = 0; ii < num_samps; ii++) {
+    vec += Eigen::Matrix<double, N, 1>::Random();
+  }
+  vec /= 2;
 }
 
 template<int N>
 Eigen::Matrix<double, N, 1> randn(const Eigen::Matrix<double, N, N> & cov)
 {
-  int num_samps = 12;
-  //first get independent gaussian vector
-  Eigen::Matrix<double, N, 1> vec = Eigen::Matrix<double, N, 1>::Zero();
-
-  for (int ii = 0; ii < num_samps; ii++) {
-    vec += Eigen::Matrix<double, N, 1>::Random();
-  }
-  vec /= 2;
+  Eigen::Matrix<double, N, 1> vec;
+  randn_identity(vec);
   Eigen::Matrix<double, N, N> chol_decomp = cov.llt().matrixL();
   return chol_decomp * vec;
 }
+
+template<int N>
+double normpdf(const Eigen::Matrix<double, N, 1> & x, const Eigen::Matrix<double, N, 1> & mu
+    , const Eigen::Matrix<double, N, N> & sigma)
+{
+  Eigen::Matrix<double, N, 1> diff = mu - x;
+
+  double exponent = -0.5 * diff.transpose() * sigma.ldlt().solve(diff);
+
+  return exp(exponent) / (pow(2 * M_PI,((double) N) / 2.0) * pow(sigma.determinant(), 0.5));
+}
+
+template<int N>
+void fitParticles(
+    const Eigen::Matrix<double, N, Eigen::Dynamic> & state_samples, Eigen::VectorXd & weights
+    , Eigen::Matrix<double, N, 1> & mean, Eigen::Matrix<double, N, N> & covariance)
+{
+
+  int num_samples = state_samples.cols();
+  weights /= weights.sum();
+  mean = state_samples * weights;
+
+  Eigen::Matrix<double, N, 1> diff;
+  covariance = Eigen::Matrix<double, N, N>::Zero();
+  for (int ii = 0; ii < num_samples; ii++)
+  {
+    diff = mean - state_samples.block(0, ii, N, 1);
+    covariance += diff * diff.transpose() * weights(ii);
+  }
+}
+
 }
 
 #endif
